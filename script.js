8b54d1628cc3eb60aa2566715271f9a32322edf2
yousuf mahomed <118985086+yousufmahomed@users.noreply.github.com>
2023-03-29 16:30:35 +0200

    multiple changes

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function goBack() {
   }
 
 
+  function toggleFullscreenImage(src) {
+  let overlay = document.getElementById('fullscreen-image-overlay');
+
+  if (!overlay) {
+    overlay = document.createElement('div');
+    overlay.id = 'fullscreen-image-overlay';
+    overlay.classList.add('fullscreen-image-overlay');
+    overlay.innerHTML = `<img src="${src}" alt="Full-size image" />`;
+    overlay.onclick = () => toggleFullscreenImage(null);
+    document.body.appendChild(overlay);
+  } else {
+    document.body.removeChild(overlay);
+  }
+}
+
